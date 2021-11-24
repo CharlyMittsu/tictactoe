0ad61e3a6e8db4c3529131c7a11c88b3ae91aab4
Charles Blain <74670182+CharlyMittsu@users.noreply.github.com>
2021-11-24 10:52:27 +0100

    def

--- tictactoe.py
+++ tictactoe.py
@@ -44,6 +44,7 @@ def jeu(x):
 #______(4)Verification victoire
 #____________(4.1)Verification ligne verticale
 def verification():
+    combo=0
     for i in range (0,taille_x):
         
         if combo < combo_max:
@@ -54,14 +55,12 @@ def verification():
                     if case != 0 :
                         combo = combo+1
                 else :
-                    if case == tableau[case-taille_x] :
+                    if case == tableau[(j-1)*taille_x+i] :
                         combo = combo+1
                     else :
                         combo = 0
-        elif case == 1:
-            win_X = True
-        elif case == 2:
-            win_O = True
+
+        
                     
 #____________(4.2)Verification ligne horizontale
     for i in range (0,taille_y):
@@ -74,19 +73,16 @@ def verification():
                     if case != 0 :
                         combo = combo+1
                 else :
-                    if case == tableau[case-1] :
+                    if case == tableau[i*taille_x+j-1] :
                         combo = combo+1
                     else :
                         combo = 0
-        elif case == 1:
-            win_X = True
-        elif case == 2:
-            win_O = True
+
 
 #____________(4.3)Verification diagonale
     
     
-    for i in range (0,grand-(petit-1)):
+    for i in range (0,grand-(combo_max-1)):#"petit" pour les tableaux verticaux
         for j in range (0,taille_x):
             if combo < combo_max:
                 combo=0
@@ -95,37 +91,43 @@ def verification():
                     if case != 0 :
                         combo = combo+1
                 else :
-                    if case == tableau[case-1] :
+                    if case == tableau[i+(j-1)*taille_x+j] :
                         combo = combo+1
                     else :
                         combo = 0
         for j in range (0,taille_x):
             if combo < combo_max:
                 combo=0
-                case = tableau[i+j*taille_x+j]
+                case = tableau[i+combo_max+j*taille_x-j]
                 if combo == 0 :
                     if case != 0 :
                         combo = combo+1
                 else :
-                    if case == tableau[case-1] :
+                    if case == tableau[i+combo_max+(j-1)*taille_x-j] :
                         combo = combo+1
                     else :
                         combo = 0
-            elif case == 1:
-                win_X = True
-            elif case == 2:
-                win_O = True
+    if combo < combo_max:
+        return (case)
+    else :
+        return 0        
+    
 
 #______(5)Verification grille complète
+def complet():
     fini = True
     for i in range (0,taille_x*taille_y):
         if tableau [i] == 0:
             fini = False
+    return fini
+    
+    
         
 
 #______(6)JEU
 while win_O == False and win_X == False and fini == False :
     affichage()
+
     if tour == True :
         print("Au tour du joueur O")
         jeu(2)
@@ -134,7 +136,13 @@ while win_O == False and win_X == False and fini == False :
         print("Au tour du joueur X")
         jeu(1)
         tour = True
-    verification()
+    case = verification()
+    if case == 1:
+                X = True
+    elif case == 2:
+        O = True
+    fini = complet()
+    
 if win_O == True:
     print("Le joueur O à gagné !")
 elif win_X == True :
